b86832dcf68e99248f53a515455f53fbdaaadc83
tejvirus0003 <64889123+tejvirus0003@users.noreply.github.com>
2021-03-13 13:06:32 +0530

    Add files via upload

---
+++ StudyMaterial.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Material from "./Material";
+export default class StudyMaterial extends React.Component {
+  render = () => {
+    return (
+      <div>
+        <Material title="database" branch="C.S.E" author="tejus kaw" />
+        <Material title="database" branch="C.S.E" author="tejus kaw" />
+        <Material title="database" branch="C.S.E" author="tejus kaw" />
+        <Material title="database" branch="C.S.E" author="tejus kaw" />
+        <Material title="database" branch="C.S.E" author="tejus kaw" />
+        <Material title="database" branch="C.S.E" author="tejus kaw" />
+        <Material title="database" branch="C.S.E" author="tejus kaw" />
+        <Material title="database" branch="C.S.E" author="tejus kaw" />
+      </div>
+    );
+  };
+}
