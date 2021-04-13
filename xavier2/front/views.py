69021da6fe8b666958f5e92907bc990dbe9baed7
unknown <tusharkumarshiva.bt19cse@pec.edu.in>
2021-04-13 16:59:00 +0530

    installed manual user creation

--- xavier2/front/views.py
+++ xavier2/front/views.py
@@ -6,11 +6,26 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login,logout
 from django import forms
 from django.http import HttpResponseRedirect
-from django.contrib.auth.forms import UserCreationForm , AuthenticationForm
+from django.contrib.auth.forms import  AuthenticationForm , UserCreationForm
 
 
 
-class SendMessage(forms.ModelForm):
+class UserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
+
+    def save(self, commit=True):
+        user = super(UserCreationForm, self).save(commit=False)
+        user.email = self.cleaned_data["email"]
+        if commit:
+            user.save()
+        return user
+
+
+class SendComment(forms.ModelForm):
 	class Meta :
 		model = Comment
 		fields = ['content']
@@ -32,30 +47,44 @@ def discuss(request):
 def sign(request):
 
     if request.method == 'GET':
-        form = AuthenticationForm()
-        return render(request, 'front/sign.html', {'form' : form })
+        form1 = AuthenticationForm()
+        form2 = UserCreationForm()
+        return render(request, 'front/sign.html', {'form1' : form1 , 'form2' : form2 })
 
     if request.method == 'POST':
 
         if request.POST['action'] == 'login':
-        	form = AuthenticationForm(request=request, data=request.POST)
-        	if form.is_valid():
-        	    username = form.cleaned_data.get('username')
-        	    password = form.cleaned_data.get('password')
+        	form1 = AuthenticationForm(request=request, data=request.POST)
+        	form2 = UserCreationForm()
+        	if form1.is_valid():
+        	    username = form1.cleaned_data.get('username')
+        	    password = form1.cleaned_data.get('password')
         	    user = authenticate(username=username, password=password)
         	    if user is not None:
         	        login(request, user)
         	        return redirect('chatpage')
         	    else:
-        	        form.error_messages['nouser']= _('No such user found')
+        	        form1.error_messages['nouser']= _('No such user found')
         	        raise forms.ValidationError(
-        	        form.error_messages['nouser'],
+        	        form1.error_messages['nouser'],
         	        code='nouser',)
-        	        return render(request, 'front/sign.html', { 'form': form })
+        	        return render(request, 'front/sign.html', {'form1' : form1 , 'form2' : form2 })
         	else:
         	    # If there were errors, we render the form with these
         	    # errors
-        	    return render(request, 'front/sign.html', { 'form': form }) 
+        	    return render(request, 'front/sign.html', {'form1' : form1 , 'form2' : form2 }) 
+
+
+        if request.POST['action'] == 'signup':
+        	form1 = AuthenticationForm()
+        	form2 = UserCreationForm(request.POST)
+        	if form2.is_valid():
+        	    form2.save()
+        	    return redirect('signpage')
+
+        	else:
+        	    return render(request, 'front/sign.html', {'form1' : form1 , 'form2' : form2 }) 
+
 
 
   
@@ -74,7 +103,7 @@ def chat(request):
 
 	if request.method == 'POST':
 
-			send = SendMessage(request.POST )
+			send = SendComment(request.POST )
 			if send.is_valid():
 				Comment.objects.create(author=request.user , content=send.cleaned_data['content'])
 				return redirect('chatpage')
@@ -88,7 +117,7 @@ def chat(request):
 
 
 
-	send = SendMessage()
+	send = SendComment()
 	return render(request, 'front/chat.html', {'comments': comments   , 'send' : send})
 
 
